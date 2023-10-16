401eac68f2bca56f49d4d718e3b5ac1652156a6c
Radosław Gaładyk <rgaladyk@creatuity.com>
2023-10-16 21:23:17 +0200

    all without qunatity

---
+++ src/components/organisms/TopNav/TopNav.tsx
@@ -0,0 +1,40 @@
+import { type Route } from "next";
+import staticData from "./TopNavData.json";
+import { ActiveLink } from "@/components/atoms/ActiveLink/ActiveLink";
+import { SearchBar } from "@/components/molecules/SearchBar/SearchBar";
+import { type TopNavItemType, type TopNavType } from "@/components/types";
+
+export const TopNav = async () => {
+	const data: TopNavType = staticData.map((links) => {
+		return {
+			path: links.path as Route,
+			name: links.name,
+		};
+	});
+
+	// QUANTITY here
+	const quantity = 0;
+
+	return (
+		<nav className="mx-auto max-w-xl" role="navigation">
+			<div className="flex items-center">
+				<ul className="flex text-center">
+					{data.map((link: TopNavItemType, idx: number) => {
+						return (
+							<li key={idx} className="m-4 inline-block text-xl font-semibold hover:underline">
+								<ActiveLink
+									href={link.path}
+									text={link.name}
+									className="text-blue-400"
+									activeClassName="text-blue-600 border-b-2 border-solid border-green-500"
+								/>
+							</li>
+						);
+					})}
+				</ul>
+				<SearchBar />
+				{quantity}
+			</div>
+		</nav>
+	);
+};
